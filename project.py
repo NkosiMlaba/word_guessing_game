42fc7fde4d8752033244daf7c7e6ce7f95435aed
Nkosikhona Mlaba <nmlaba023@student.wethinkcode.co.za>
2024-09-13 08:57:26 +0200

    added docstrings to the functions

--- project.py
+++ project.py
@@ -14,6 +14,17 @@ class Colors:
 
 
 def update_user_guess(word, user_word_progress, user_letter_guess):
+    """
+    Update the user's progress in guessing the word.
+
+    Parameters:
+    word (str): The correct word to be guessed.
+    user_word_progress (str): The current progress of the user's guess.
+    user_letter_guess (str): The letter guessed by the user.
+
+    Returns:
+    str: The updated progress of the user's guess.
+    """
     updated_word = ""
     for index, letter in enumerate(word):
         if letter == user_letter_guess:
@@ -25,6 +36,16 @@ def update_user_guess(word, user_word_progress, user_letter_guess):
 
 
 def get_missing_letters(correct_word, user_word_progress):
+    """
+    Get the missing letters in the correct word based on the user's progress.
+
+    Parameters:
+    correct_word (str): The correct word to be guessed.
+    user_word_progress (str): The current progress of the user's guess.
+
+    Returns:
+    str: The missing letters in the correct word.
+    """
     missing_letters = ""
     for index, letter in enumerate(correct_word):
         if letter == user_word_progress[index]:
@@ -36,8 +57,17 @@ def get_missing_letters(correct_word, user_word_progress):
 
 
 def load_words_chosen_category(category):
+    """
+    Load the words from the chosen category.
+
+    Parameters:
+    category (str): The chosen category for the hangman game.
+
+    Returns:
+    list: A list of words from the chosen category.
+    """
     path = category.lower()
-    
+
     try:
         with open(f"words/{path}.txt", 'r') as file:
             words = file.read().strip().split('\n')
@@ -49,10 +79,22 @@ def load_words_chosen_category(category):
 
 
 def show_disclaimer():
+    """
+    Show the disclaimer message.
+
+    Returns:
+    str: The disclaimer message.
+    """
     return "You can exit this application by typing 'exit' or 'quit'\n"
 
 
 def show_categories():
+    """
+    Show the available categories for the hangman game.
+
+    Returns:
+    str: The available categories for the hangman game.
+    """
     files = [f.replace('.txt', '').capitalize() for f in os.listdir('words') if f.endswith('.txt')]
     
     output = "These are the available categories for the hangman words:\n"
@@ -63,6 +105,12 @@ def show_categories():
 
 
 def get_user_category():
+    """
+    Get the user's chosen category for the hangman game.
+
+    Returns:
+    tuple: A tuple containing the user's chosen category and a message indicating the chosen category.
+    """
     files = [f.replace('.txt', '').lower() for f in os.listdir('words') if f.endswith('.txt')]
     categories = {str(index): file for index, file in enumerate(files, start=1)}
 
@@ -83,15 +131,33 @@ def get_user_category():
 
 
 def show_goodbye():
+    """
+    Show the goodbye message.
+
+    Returns:
+    str: The goodbye message.
+    """
     return pyfiglet.figlet_format("\nGOODBYE, THANKS FOR PLAYING", font = "mini" ) 
 
 
 def start_program():
+    """
+    Start the hangman game program.
+
+    Returns:
+    str: The welcome message for the hangman game.
+    """
     return pyfiglet.figlet_format("WELCOME  TO  HANGMAN", font = "mini" ) 
-    
 
 
 def print_with_color(text, color):
+    """
+    Print the text with the specified color.
+
+    Parameters:
+    text (str): The text to be printed.
+    color (str): The color of the text.
+    """
     if color == "red":
         print(Colors.RED + text + Colors.CYAN)
     elif color == "green":
@@ -108,6 +174,12 @@ def print_with_color(text, color):
 
 
 def load_stickman():
+    """
+    Load the stickman drawings for the hangman game.
+
+    Returns:
+    list: A list of stickman drawings.
+    """
     try:
         with open("stickman/stickman5.txt", 'r') as file:
             drawings = file.read().strip().split('\n\n')
@@ -120,6 +192,9 @@ def load_stickman():
 
 
 def main():
+    """
+    The main function to run the hangman game.
+    """
     print(Colors.CYAN)
     print(start_program())
     print(show_disclaimer())
@@ -218,7 +293,6 @@ def main():
             print_with_color(text, "green")
             print(show_goodbye())
             exit()
-
 
 
 if __name__ == "__main__":
